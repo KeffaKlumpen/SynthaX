8b863a4cbda845d45f4f076d58beffd71823f4d1
LektorVinberg <97341150+LektorVinberg@users.noreply.github.com>
2022-04-07 15:25:40 +0200

    Updates to synthax-view.fxml
    Co-Authored-By: twegestal <96296150+twegestal@users.noreply.github.com>

--- src/main/java/com/synthax/SynthaX/ADSR.java
+++ src/main/java/com/synthax/SynthaX/ADSR.java
@@ -3,24 +3,39 @@ package com.synthax.SynthaX;
 import net.beadsproject.beads.core.AudioContext;
 import net.beadsproject.beads.ugens.Envelope;
 
+/**
+ * Class for controlling the amplitude through an ADSR-envelope.
+ * @author Viktor Lenberg
+ * @author Teodor Wegestål
+ */
 public class ADSR {
     private Envelope envelope;
-    private float attackValue;
-    private float decayValue;
+    private float attackValue = 1;
+    private float decayValue = 1;
     private float sustainValue = 1;
-    private float releaseValue;
+    private float releaseValue = 1;
     private float peakGain = 0.5f;
 
     public ADSR (AudioContext ac) {
         envelope = new Envelope(ac, 0.0f);
     }
 
+    /**
+     * Routes the playing signal through the attack, and decay segments of the envelope.
+     * @author Viktor Lenberg
+     * @author Teodor Wegestål
+     */
     public void attackDecay() {
         envelope.clear();
         envelope.addSegment(peakGain, attackValue);
-        envelope.addSegment(sustainValue, decayValue);
+        envelope.addSegment(peakGain * sustainValue, decayValue);
     }
 
+    /**
+     * Routes the playing signal through the release segment of the envelope.
+     * @author Viktor Lenberg
+     * @author Teodor Wegestål
+     */
     public void release() {
         envelope.clear();
         envelope.addSegment(0.0f, releaseValue);
@@ -36,10 +51,6 @@ public class ADSR {
 
     public void setSustainValue(float sustainValue) {
         this.sustainValue = sustainValue;
-    }
-
-    public float getSustainValue() {
-        return sustainValue;
     }
 
     public void setReleaseValue(float releaseValue) {
