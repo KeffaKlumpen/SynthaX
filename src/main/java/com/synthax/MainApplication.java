3c9dda1ba960683b8f58db1e4859ce1ec7c12af7
Axel nilsson <axel.nilsson01@gmail.com>
2022-04-28 23:09:48 +0200

    new fonts

--- src/main/java/com/synthax/MainApplication.java
+++ src/main/java/com/synthax/MainApplication.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Font.loadFont(MainApplication.class.getResource("fonts/wonome.ttf").toExternalForm(), 10);
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("view/synthax-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(getClass().getResource("skins.css").toExternalForm());
