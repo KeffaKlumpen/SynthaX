074c240f00b4326ff41653395ea4c6f97559f6c2
KeffaKlumpen <joeles76@gmail.com>
2022-05-04 14:42:12 +0200

    Implemented stacked filters (LP and HP)

--- src/main/java/com/synthax/model/EQFilters.java
+++ src/main/java/com/synthax/model/EQFilters.java
@@ -11,76 +11,82 @@ import net.beadsproject.beads.core.UGen;
 import net.beadsproject.beads.ugens.BiquadFilter;
 
 public class EQFilters {
-    private final BiquadFilter filterHP;
-    private final BiquadFilter filterHP2;
+    private static final float HP_DISABLED_FREQ = 50f;      // value where the filter is effectively disabled.
+    private static final float LP_DISABLED_FREQ = 22000f;   // value where the filter is effectively disabled.
+    private static final int FILTER_COUNT = 16;
+
+    private final BiquadFilter[] highPassFilters;
     private final BiquadFilter filterNotch;
-    private final BiquadFilter filterLP;
+    private final BiquadFilter[] lowPassFilters;
 
     // High-pass
-    private final float hpMinFreq = 800f;
-    private final float hpMaxFreq = 2500f;
-    private final float HPdisableFreq = 0.001f;     // value where the filter is effectively disabled.
+    private final float hpMinFreq = 400f;
+    private final float hpMaxFreq = 2000f;
     private boolean hpActive = false;
     private float savedHPCutoff = hpMinFreq;
     // Low-pass
-    private final float lpMinFreq = 50f;
+    private final float lpMinFreq = 100f;
     private final float lpMaxFreq = 1500f;
-    private final float LPdisableFreq = 22000f; // value where the filter is effectively disabled.
     private boolean lpActive = false;
     private float savedLPCutoff = lpMinFreq;
 
     public EQFilters() {
-        filterHP = new BiquadFilter(1, BiquadFilter.BUTTERWORTH_HP);
-        filterHP.setFrequency(HPdisableFreq);
-        filterHP2 = new BiquadFilter(1, BiquadFilter.BUTTERWORTH_HP);
-        filterHP2.setFrequency(HPdisableFreq);
+        highPassFilters = new BiquadFilter[FILTER_COUNT];
+        for (int i = 0; i < FILTER_COUNT; i++) {
+            BiquadFilter filter = new BiquadFilter(1, BiquadFilter.BESSEL_HP);
+            filter.setFrequency(HP_DISABLED_FREQ);
+
+            // set up chaining
+            if(i > 0) {
+                filter.addInput(highPassFilters[i - 1]);
+            }
+            
+            highPassFilters[i] = filter;
+        }
 
         filterNotch = new BiquadFilter(1, BiquadFilter.NOTCH);
         //filterNotch.addInput(filterHP);
         //filterNotch.setGain(1f);
 
-        filterLP = new BiquadFilter(1, BiquadFilter.LP);
-        filterLP.addInput(filterHP);
-        filterLP.setFrequency(LPdisableFreq);
+        lowPassFilters = new BiquadFilter[FILTER_COUNT];
+        for (int i = 0; i < FILTER_COUNT; i++) {
+            BiquadFilter filter = new BiquadFilter(1, BiquadFilter.BESSEL_LP);
+            filter.setFrequency(LP_DISABLED_FREQ);
+
+            // set up chaining
+            if(i > 0) {
+                filter.addInput(lowPassFilters[i - 1]);
+            }
+
+            lowPassFilters[i] = filter;
+        }
+        lowPassFilters[0].addInput(highPassFilters[FILTER_COUNT - 1]);
     }
 
     public void setHPCutoff(float cutoff) {
         float mapped = BasicMath.map(cutoff, 0f, 1f, hpMinFreq, hpMaxFreq);
         if(hpActive) {
-            filterHP.setFrequency(mapped);
-            filterHP2.setFrequency(mapped);
+            setHPfreq(mapped);
         } else {
             savedHPCutoff = mapped;
-            System.out.println("Saved: " + savedHPCutoff);
+            System.out.println("Saved HP: " + savedHPCutoff);
         }
     }
 
-    boolean once = true;
     public void setHPSlope(float slope) {
         float mapped = BasicMath.map(slope, 0f, 1f, 0.1f, 1f);
-        filterHP.setQ(mapped);
-
-        if(once){
-            filterHP2.addInput(filterHP);
-            filterLP.clearInputConnections();
-            filterLP.addInput(filterHP2);
-            once = false;
-        }
+        // filterHP.setQ(mapped);
     }
 
     public void setHPActive(boolean newActive) {
         hpActive = newActive;
 
         if(hpActive) {
-            filterHP.setFrequency(savedHPCutoff);
-            filterHP2.setFrequency(savedHPCutoff);
+            setHPfreq(savedHPCutoff);
         } else {
-            savedHPCutoff = filterHP.getFrequency();
-            System.out.println("Saved: " + savedHPCutoff);
-            filterHP.setFrequency(HPdisableFreq);
-            filterHP.reset();
-            filterHP2.setFrequency(HPdisableFreq);
-            filterHP2.reset();
+            savedHPCutoff = highPassFilters[0].getFrequency();
+            System.out.println("Saved HP: " + savedHPCutoff);
+            setHPfreq(HP_DISABLED_FREQ);
         }
     }
 
@@ -101,34 +107,53 @@ public class EQFilters {
     public void setLPCutoff(float cutoff) {
         float mapped = BasicMath.map(cutoff, 0f, 1f, lpMinFreq, lpMaxFreq);
         if(lpActive) {
-            filterLP.setFrequency(mapped);
+            setLPfreq(mapped);
         } else {
             savedLPCutoff = mapped;
+            System.out.println("Saved LP: " + savedHPCutoff);
         }
     }
 
     public void setLPSlope(float slope) {
         float mapped = BasicMath.map(slope, 0f, 1f, 0.1f, 1f);
-        filterLP.setQ(mapped);
+        //filterLP.setQ(mapped);
     }
 
     public void setLPActive(boolean newActive) {
         lpActive = newActive;
 
         if(lpActive) {
-            filterLP.setFrequency(savedLPCutoff);
+            setLPfreq(savedLPCutoff);
         } else {
-            savedLPCutoff = filterLP.getFrequency();
-            filterLP.setFrequency(LPdisableFreq);
-            filterLP.reset();
+            savedLPCutoff = lowPassFilters[0].getFrequency();
+            System.out.println("Saved LP: " + savedLPCutoff);
+            setLPfreq(LP_DISABLED_FREQ);
+        }
+    }
+
+    private void setHPfreq(float freq) {
+        System.out.println("Setting HP freq: " + freq);
+        for (int i = 0; i < FILTER_COUNT; i++) {
+            highPassFilters[i].setFrequency(freq);
+        }
+    }
+
+    private void setLPfreq(float freq) {
+        System.out.println("Setting LP freq: " + freq);
+        for (int i = 0; i < FILTER_COUNT; i++) {
+            lowPassFilters[i].setFrequency(freq);
         }
     }
 
     public void addInput(UGen source) {
-        filterHP.addInput(source);
+        if(highPassFilters[0] != null) {
+            highPassFilters[0].addInput(source);
+        } else {
+            System.err.println("EQFilters.addInput(): Cannot add input, no filter found!");
+        }
     }
 
     public UGen getOutput() {
-        return filterLP;
+        return lowPassFilters[FILTER_COUNT - 1];
     }
 }
