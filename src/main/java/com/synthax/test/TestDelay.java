4c5d026ddfe5d68ef52a21326335a47732e1eb0a
LektorVinberg <97341150+LektorVinberg@users.noreply.github.com>
2022-05-04 12:36:27 +0200

    Hittade lösning för att kontrollera feedback
    Vi gör en liten fuling med en tråd ;) hoppas CPU:n inte blir allt för ledsen.
    
    Co-Authored-By: twegestal <96296150+twegestal@users.noreply.github.com>

--- src/main/java/com/synthax/test/TestDelay.java
+++ src/main/java/com/synthax/test/TestDelay.java
@@ -8,6 +8,10 @@ import net.beadsproject.beads.ugens.*;
 import java.util.Scanner;
 
 public class TestDelay {
+    private float feedbackTime; //detta är en go lösning på feedbackparametern
+    private Gain delayGain;
+    private float delayGainValue = 0.5f;
+    private Glide delayGainGlide;
 
     public TestDelay() {
         JavaSoundAudioIO jsaio = new JavaSoundAudioIO(512);
@@ -15,10 +19,11 @@ public class TestDelay {
         AudioContext.setDefaultContext(ac);
 
         //DELAY_01:
-        delay01(ac);
+        //feedBackMedEnvelopeFunkarLiksomInteBuhu(ac);
+
         //randomSynthFrånHemsidan(ac);
 
-
+        testaFeedbackMedTypEnSleepKanske(ac);
     }
 
     private void randomSynthFrånHemsidan(AudioContext ac) {
@@ -36,7 +41,7 @@ public class TestDelay {
         delayIn.addInput(delayGain);
     }
 
-    public void delay01(AudioContext ac) {
+    public void feedBackMedEnvelopeFunkarLiksomInteBuhu(AudioContext ac) {
         Glide modulatorFrequency = new Glide(ac, 20, 30);
         WavePlayer modulator = new WavePlayer(ac, modulatorFrequency, Buffer.SINE);
 
@@ -47,7 +52,7 @@ public class TestDelay {
             }
         };
 
-        WavePlayer carrier = new WavePlayer(ac, 440f, Buffer.SINE);
+        WavePlayer carrier = new WavePlayer(ac, 440f, Buffer.TRIANGLE);
 
         Envelope gainEnvelope = new Envelope(ac, 0.0f);
 
@@ -60,9 +65,9 @@ public class TestDelay {
         delayIn.addInput(synthGain);
 
 
-        TapOut delayOut = new TapOut(ac, delayIn, 299); //delayvärdet här styr time
+        TapOut delayOut = new TapOut(ac, delayIn, 400); //delayvärdet här styr time
 
-        Gain delayGain = new Gain(ac, 1, 0.6f); //gainvärdet här styr amplitud på ekot
+        Gain delayGain = new Gain(ac, 1, 1f); //gainvärdet här styr amplitud på ekot
 
         //OLLIES SKIT
         /*
@@ -71,29 +76,117 @@ public class TestDelay {
         myGain.addInput(delayGain);
         delayIn.addInput(myGain);*/
 
+        //VÅRAN SKIT
+
+        Envelope feedBackEnv = new Envelope(ac, 0.0f);
+        Gain feedBackGain = new Gain(ac, 1, feedBackEnv);
+        feedBackGain.addInput(delayGain);
+        delayIn.addInput(feedBackGain);
+
+        //END VÅRAN SKIT
 
         delayGain.addInput(delayOut);
 
+
         //to feed the delay back into itself, uncomment this line:
         //delayIn.addInput(delayGain);
+
 
         ac.out.addInput(synthGain);
         ac.out.addInput(delayGain);
         ac.start();
 
-
-        int kuken = 0;
+        float feedBackDuration = 2000f;
         while (true) {
+            feedBackEnv.addSegment(1.0f, 1f);
+            feedBackEnv.addSegment(0f, feedBackDuration);
             gainEnvelope.addSegment(0.8f, 10);
             gainEnvelope.addSegment(0, 300);
             try {
-                Thread.sleep(3000);
+                Thread.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             Scanner scn = new Scanner(System.in);
-            //myGlide.setValue(scn.nextFloat());
-            kuken++;
+            feedBackDuration = scn.nextFloat();
+            //delayOut.setDelay(scn.nextFloat());
+        }
+    }
+
+    public void testaFeedbackMedTypEnSleepKanske(AudioContext ac) {
+        Glide modulatorFrequency = new Glide(ac, 20, 30);
+        WavePlayer modulator = new WavePlayer(ac, modulatorFrequency, Buffer.SINE);
+
+        Function frequencyModulation = new Function(modulator) {
+            @Override
+            public float calculate() {
+                return (x[0] * 100.0f) + 440f; //+mouseY
+            }
+        };
+
+        WavePlayer carrier = new WavePlayer(ac, 440f, Buffer.TRIANGLE);
+
+        Envelope gainEnvelope = new Envelope(ac, 0.0f);
+
+        Gain synthGain = new Gain(ac, 1, gainEnvelope);
+
+        synthGain.addInput(carrier);
+
+        TapIn delayIn = new TapIn(ac, 2000.0f);
+
+        delayIn.addInput(synthGain);
+
+
+        TapOut delayOut = new TapOut(ac, delayIn, 400); //delayvärdet här styr time
+
+        delayGainGlide = new Glide(ac,delayGainValue, 200.0f);
+        delayGain = new Gain(ac, 1, delayGainGlide); //gainvärdet här styr amplitud på ekot
+
+        //OLLIES SKIT
+        /*
+        Glide myGlide = new Glide(ac, 0);
+        Gain myGain = new Gain(ac, 1, myGlide);
+        myGain.addInput(delayGain);
+        delayIn.addInput(myGain);*/
+
+        delayGain.addInput(delayOut);
+
+
+        //to feed the delay back into itself, uncomment this line:
+        delayIn.addInput(delayGain);
+
+
+        ac.out.addInput(synthGain);
+        ac.out.addInput(delayGain);
+        ac.start();
+
+        feedbackTime = 2000f;
+        while (true) {
+            delayGainGlide.setValue(delayGainValue);
+            new FeedBackSleepyHead();
+            gainEnvelope.addSegment(0.8f, 10);
+            gainEnvelope.addSegment(0, 300);
+
+            Scanner scn = new Scanner(System.in);
+            feedbackTime = scn.nextFloat();
+            //delayOut.setDelay(scn.nextFloat());
+        }
+    }
+
+    private class FeedBackSleepyHead implements Runnable {
+
+        public FeedBackSleepyHead() {
+            new Thread(this).start();
+        }
+        @Override
+        public void run() {
+            try {
+                Thread.sleep((long)feedbackTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            delayGainGlide.setValue(0.0f);
         }
     }
 
