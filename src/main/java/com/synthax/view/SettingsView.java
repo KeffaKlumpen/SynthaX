b510941175969b1ac21227416ba86853dd82b7ed
Axel nilsson <axel.nilsson01@gmail.com>
2022-05-18 15:23:16 +0200

    presetslist
    Co-Authored-By: LEales <102584475+LEales@users.noreply.github.com>

--- src/main/java/com/synthax/view/SettingsView.java
+++ src/main/java/com/synthax/view/SettingsView.java
@@ -1,4 +1,51 @@
 package com.synthax.view;
 
-public class SettingsView {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SettingsView implements Initializable {
+    @FXML private Button btnDelete;
+    @FXML private VBox presetsList;
+
+    @FXML
+    public void onActionDelete() {
+        for (int i = 0; i < presetsList.getChildren().size(); i++) {
+            CheckBox c = (CheckBox) presetsList.getChildren().get(i);
+            if (c.isSelected()) {
+                presetsList.getChildren().remove(i);
+                i--;
+            }
+        }
+    }
+
+    private void populatePresetsBox() {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        presetsList.getChildren().addAll(new CheckBox("Luke"),
+                new CheckBox("Luke"),
+                new CheckBox("Luke"),
+                new CheckBox("Luke"),
+                new CheckBox("Luke"),
+                new CheckBox("Luke"),
+                new CheckBox("Lukeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"),
+                new CheckBox("Luke"),
+                new CheckBox("Luke"),
+                new CheckBox("Luke"),
+                new CheckBox("Luke"),
+                new CheckBox("Luke"),
+                new CheckBox("Luke"),
+                new CheckBox("Luke"));
+
+
+    }
 }
