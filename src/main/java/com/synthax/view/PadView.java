420a539c3840fea77b33f4d0bee03745bf691ff2
twegestal <96296150+twegestal@users.noreply.github.com>
2022-05-20 14:32:52 +0200

    SamplePlayer looks
    Co-Authored-By: axNil <102180390+axNil@users.noreply.github.com>

--- src/main/java/com/synthax/view/PadView.java
+++ src/main/java/com/synthax/view/PadView.java
@@ -1,6 +1,7 @@
 package com.synthax.view;
 
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,5 +12,8 @@ public class PadView implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void onActionReverbBypass(MouseEvent mouseEvent) {
     }
 }
