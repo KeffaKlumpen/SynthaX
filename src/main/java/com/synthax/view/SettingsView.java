57ea604519d38fcd104cee88dd6ff2ca0a5c385b
LEales <102584475+LEales@users.noreply.github.com>
2022-05-20 12:25:34 +0200

    Update SettingsView.java

--- src/main/java/com/synthax/view/SettingsView.java
+++ src/main/java/com/synthax/view/SettingsView.java
@@ -2,12 +2,11 @@ package com.synthax.view;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Spinner;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -20,15 +19,18 @@ public class SettingsView implements Initializable {
 
     @FXML
     public void onActionDelete() {
-        for (int i = 0; i < presetsList.getChildren().size(); i++) {
-            CheckBox c = (CheckBox) presetsList.getChildren().get(i);
-            if (c.isSelected()) {
-                presetsList.getChildren().remove(i);
-                synthaxView.deletePreset(c.getText());
-                i--;
+        Alert a = new Alert(Alert.AlertType.CONFIRMATION);
+        if (a.showAndWait().get() == ButtonType.OK) {
+            for (int i = 0; i < presetsList.getChildren().size(); i++) {
+                CheckBox c = (CheckBox) presetsList.getChildren().get(i);
+                if (c.isSelected()) {
+                    presetsList.getChildren().remove(i);
+                    synthaxView.deletePreset(c.getText());
+                    i--;
+                }
             }
+            synthaxView.updateSequencerPresetList();
         }
-        synthaxView.updateSequencerPresetList();
     }
 
     @FXML
