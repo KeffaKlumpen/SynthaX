de88b2627fa219d992a3e6c39c6bdaa4335e905b
twegestal <96296150+twegestal@users.noreply.github.com>
2022-05-22 08:41:58 +0200

    More sound

--- src/main/java/com/synthax/sample_player/controller/SamplePlayerController.java
+++ src/main/java/com/synthax/sample_player/controller/SamplePlayerController.java
@@ -33,34 +33,52 @@ public class SamplePlayerController {
 
     private void initPads() {
         String sourceClap = "src/main/resources/com/synthax/samples/clap.wav";
-        String sourceHiHat = "src/main/resources/com/synthax/samples/hihat.wav";
+        String sourceClHat = "src/main/resources/com/synthax/samples/ClHat.wav";
         String sourceKick = "src/main/resources/com/synthax/samples/kick.wav";
         String sourceSnare = "src/main/resources/com/synthax/samples/snare.wav";
+        String sourceCrash = "src/main/resources/com/synthax/samples/crash.wav";
+        String sourceRide = "src/main/resources/com/synthax/samples/ride.wav";
+        String sourceTom1 = "src/main/resources/com/synthax/samples/tom1.wav";
+        String sourceTom2 = "src/main/resources/com/synthax/samples/tom2.wav";
+        String sourceOpHat = "src/main/resources/com/synthax/samples/OpHat.wav";
         for (int i = 0; i < padCount; i++) {
             if (i == 0) {
-                pads[i] = new Pad(sourceClap, this, i+1);
-            } else if (i == 1) {
-                pads[i] = new Pad(sourceHiHat, this, i+1);
-            } else if (i == 2) {
                 pads[i] = new Pad(sourceKick, this, i+1);
+            } else if (i == 1) {
+                pads[i] = new Pad(sourceSnare, this, i+1);
+            } else if (i == 2) {
+                pads[i] = new Pad(sourceClap, this, i+1);
             } else if (i == 3) {
-                pads[i] = new Pad(sourceSnare, this, i+1);
+                pads[i] = new Pad(sourceClHat, this, i+1);
+            } else if (i == 4) {
+                pads[i] = new Pad(sourceOpHat, this, i+1);
+            } else if (i == 5) {
+                pads[i] = new Pad(sourceTom1, this, i+1);
+            } else if (i == 6) {
+                pads[i] = new Pad(sourceTom2, this, i+1);
+            } else if (i == 7) {
+                pads[i] = new Pad(sourceCrash, this, i+1);
             } else {
-                pads[i] = new Pad(sourceSnare, this, i+1);
+                pads[i] = new Pad(sourceRide, this, i+1);
             }
         }
         for (Pad p : pads) {
             masterGain.addInput(p.getPadOutput());
         }
-        fillSourceMap(sourceClap, sourceHiHat, sourceKick, sourceSnare);
+        fillSourceMap(sourceClap, sourceClHat, sourceKick, sourceSnare, sourceOpHat, sourceTom1, sourceTom2, sourceCrash, sourceRide);
     }
 
-    private void fillSourceMap(String sourceClap, String sourceHiHat, String sourceKick, String sourceSnare) {
+    private void fillSourceMap(String sourceClap, String sourceClHat, String sourceKick, String sourceSnare, String sourceOpHat, String sourceTom1, String sourceTom2, String sourceCrash, String sourceRide) {
         sourceMap = new HashMap<>();
         sourceMap.put(sourceClap.substring(sourceClap.lastIndexOf("/") + 1, sourceClap.lastIndexOf(".")), sourceClap);
-        sourceMap.put(sourceHiHat.substring(sourceHiHat.lastIndexOf("/") + 1 , sourceHiHat.lastIndexOf(".")), sourceHiHat);
+        sourceMap.put(sourceClHat.substring(sourceClHat.lastIndexOf("/") + 1 , sourceClHat.lastIndexOf(".")), sourceClHat);
         sourceMap.put(sourceKick.substring(sourceKick.lastIndexOf("/") + 1, sourceKick.lastIndexOf(".")), sourceKick);
         sourceMap.put(sourceSnare.substring(sourceSnare.lastIndexOf("/") + 1, sourceSnare.lastIndexOf(".")), sourceSnare);
+        sourceMap.put(sourceOpHat.substring(sourceOpHat.lastIndexOf("/") + 1, sourceOpHat.lastIndexOf(".")), sourceOpHat);
+        sourceMap.put(sourceTom1.substring(sourceTom1.lastIndexOf("/") + 1, sourceTom1.lastIndexOf(".")), sourceTom1);
+        sourceMap.put(sourceTom2.substring(sourceTom2.lastIndexOf("/") + 1, sourceTom2.lastIndexOf(".")), sourceTom2);
+        sourceMap.put(sourceCrash.substring(sourceCrash.lastIndexOf("/") + 1, sourceCrash.lastIndexOf(".")), sourceCrash);
+        sourceMap.put(sourceRide.substring(sourceRide.lastIndexOf("/") + 1, sourceRide.lastIndexOf(".")), sourceRide);
     }
 
     public void playPad(int index) {
@@ -112,7 +130,7 @@ public class SamplePlayerController {
 
     public void setAllGainValues() {
         try {
-            Thread.sleep(200);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/com/synthax/test/SampleTest.java
+++ src/main/java/com/synthax/test/SampleTest.java
@@ -39,7 +39,7 @@ public class SampleTest {
     private void init() {
         String sourceKick = "src/main/resources/com/synthax/samples/kick.wav";
         String sourceSnare = "src/main/resources/com/synthax/samples/snare.wav";
-        String sourceHiHat = "src/main/resources/com/synthax/samples/hihat.wav";
+        String sourceHiHat = "src/main/resources/com/synthax/samples/ClHat.wav";
         String sourceClap = "src/main/resources/com/synthax/samples/clap.wav";
         Sample kickSample;
         Sample snareSample;
